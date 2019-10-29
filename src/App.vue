9848e977421421406f398f18ce0e0ed2ce7966c1
kevin-messiaen <kevin.messiaen@icloud.com>
2019-10-29 21:17:42 +0100

    Add contact link in footer

--- src/App.vue
+++ src/App.vue
@@ -19,16 +19,22 @@
     <v-content>
       <Home/>
     </v-content>
+
+    <v-footer padless="true">
+      <Footer/>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import Home from './components/Home';
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Home,
+    Footer
   },
   data: () => ({
     //
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card flat tile width="100%" class="grey lighten-4 text-center">
+    <v-card-text>
+      <v-btn class="mx-4" icon href="https://github.com/kevin-messiaen" target="_blank">
+        <v-icon size="24px">mdi-github-circle</v-icon>
+      </v-btn>
+      <v-btn class="mx-4" icon href="https://www.linkedin.com/in/kevin-messiaen-074681188/" target="_blank">
+        <v-icon size="24px">mdi-linkedin-box</v-icon>
+      </v-btn>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-text>
+      &copy; {{ new Date().getFullYear() }} —
+      <strong>Kevin Messiaen</strong>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+</style>
