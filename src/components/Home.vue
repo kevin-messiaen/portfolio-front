346a9892d831ab9fb2f38178de094ba7e4ccad00
kevin-messiaen <kevin.messiaen@icloud.com>
2019-10-29 20:46:19 +0100

    add responsiveness

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,31 @@
 <template>
   <v-container>
-	<Contact/>
+    <p class="text-justify headline">Je suis un développeur fullstack spécialisé dans la téchnique.</p>
+    <v-row
+      justify="center"
+      class="d-none d-sm-flex">
+      <v-col
+        cols="9"
+        class="d-none d-sm-flex d-md-none">
+        <Contact />
+      </v-col>
+      <v-col
+        cols="6"
+        class="d-none d-md-flex">
+        <Contact />
+      </v-col>
+    </v-row>
+    <Contact class="d-flex d-sm-none"/>
   </v-container>
 </template>
 
 <script>
-import Contact from './Contact'
+import Contact from "./Contact";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Contact,
+    Contact
   }
-}
+};
 </script>
